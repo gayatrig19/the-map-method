188c76e6cc86ea5a6acdaae4ff507f6998263f92
Gayatri Ghogare <ghogaregayatri@gmail.com>
2023-12-20 20:50:12 +0000

    Add the map method challenge solution code

--- map.js
+++ map.js
@@ -55,3 +55,58 @@ const students = [
 
 const studentsWithIds = students.map(student => [student.name, student.id]);
 console.log(studentsWithIds);
+
+
+
+// Challenge Solution Code
+let students1 = [
+  {
+      name: 'John',
+      subjects: ['maths', 'english', 'cad'],
+      teacher: {maths: 'Harry', english: 'Joan', cad: 'Paul'},
+      results: {maths: 90, english: 75, cad: 87},
+  },
+  {
+      name: 'Emily',
+      subjects: ['science', 'english', 'art'],
+      teacher: {science: 'Iris', english: 'Joan', art: 'Simon'},
+      results: {science: 93, english: 80, art: 95},
+  },
+  {
+      name: 'Adam',
+      subjects: ['science', 'maths', 'art'],
+      teacher: {science: 'Iris', maths: 'Harry', art: 'Simon'},
+      results: {science: 93, maths: 77, art: 95},
+  },
+  {
+      name: 'Fran',
+      subjects: ['science', 'english', 'art'],
+      teacher: {science: 'Iris', english: 'Joan', art: 'Simon'},
+      results: {science: 93, english: 87, art: 95},
+  }
+];
+
+// single line, used different variable names so as not to clash with those below.
+let [John, ...Rest] = students1.map(itm => [itm.name, itm.results]);
+console.log(John);
+console.log(Rest);
+
+console.log('\n');
+// Or create function first and use in map
+const mapper = (itm) => [itm.name, itm.results];
+let [john1, ...rest1] = students1.map(mapper);
+console.log(john1);
+console.log(rest1);
+
+// my approach
+let [john, ...rest] = students1.map(student => [student.name, student.results]);
+console.log(john);
+console.log(rest);
+
+
+
+
+
+
+
+
